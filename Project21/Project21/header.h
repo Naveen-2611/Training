1378e395e7b5579f06e9c505e4e049f4dd6cb736
Naveenkumar <Naveenkumar.saravanakumar@trumpf.com>
2024-09-09 12:52:24 +0530

    Included test file

---
+++ Project21/Project21/header.h
@@ -0,0 +1,17 @@
+// ------------------------------------------------------------------------------------------------
+// Assignment 3 ~ A training program for new joiners at Metamation, Batch - July 2024.
+// Copyright (c) Metamation India.Naveenkumar s
+// ------------------------------------------------------------------
+// Decimal to Binary conversion and Decimal to HexaDecimal conversion
+// Header File
+// -----------------------------------------------------------------------------------------------
+#pragma once
+#include<stdio.h>
+#include<limits.h>
+#include<stdlib.h>
+#include<string.h>
+
+//Decimal to binary conversion
+char* DecimalToBinary (int n);
+//DecimalToHexadecimal conversion
+char* DecimalToHexadecimal (int num);
