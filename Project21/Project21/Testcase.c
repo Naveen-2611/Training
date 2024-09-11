46f228203662e8f71313d80287278e160b770ac8
Naveenkumar <Naveenkumar.saravanakumar@trumpf.com>
2024-09-11 10:19:05 +0530

    New changes

--- Project21/Project21/Testcase.c
+++ Project21/Project21/Testcase.c
@@ -7,31 +7,15 @@
 // ------------------------------------------------------------------------------------------------
 #include"conversionfile.h"
 
-/// <summary>Checks the user input whether its invalid or not </summary>
-int GetInteger () {
-   char input[100];
-   int number = 0;
-   char* endptr;
-   printf ("\nEnter an integer: ");
-   if (fgets (input, sizeof (input), stdin) != NULL) {
-      errno = 0;
-      number = strtol (input, &endptr, 10);
-      if (endptr == input || *endptr != '\n' && *endptr != '\0' || (number == LONG_MAX || number == LONG_MIN) && errno == ERANGE 
-          || number > INT_MAX || number <= INT_MIN || input[0] == ' ') {
-         printf ("INVALID!!!\n");
-         return INT_MIN;
-      }
-      else return number;
+void PrintResult (int input[],char* output[],char*(*func)(int)) {
+   for (int i = 0; i < 11; i++) {
+      printf ("TestCase%2d: input->%12d ", i + 1, input[i]);
+      printf ((strcmp (func (input[i]), output[i]) == 0) ? ("%11s", "pass\n") : (" %11s", "Fail\n"));
    }
-   else {
-      printf ("INVALID!!!\n");
-      return INT_MIN;
-   }
-   return 0;
 }
 
 int main () {
-   int arr[] = { -255,65535, 5125412, -592,859,-7587,865482,0,-1784587,-2147483648ll };
+   int arr[] = { -255,65535, 5125412, -592,859,-7587,865482,0,-1784587,-2147483648ll,-2147483649ll };
    //checking the value for Hexadecimal value
    char* hex[] = { "FFFFFF01",
       "0000FFFF",
@@ -42,7 +26,8 @@ int main () {
       "000D34CA",
       "00000000",
       "FFE4C4F5",
-      "0071C688" };
+      "80000000",
+      "FFFFFFFF7FFFFFFF" };
    //checking the value for Binaryvalue
    char* binary[] = { "11111111111111111111111100000001",
       "00000000000000001111111111111111",
@@ -53,30 +38,36 @@ int main () {
       "00000000000011010011010011001010",
       "00000000000000000000000000000000",
       "11111111111001001100010011110101",
-      "00000000011100011100011010001000" };
+      "10000000000000000000000000000000",
+   "1111111111111111111111111111111101111111111111111111111111111111" };
 
    printf ("*****DecimaltoBinary*****\n");
-   for (int i = 0; i < 10; i++) {
-      char* binaryOutput = DecimalToBinary (arr[i]);
-      printf ("TestCase%2d: input->%12d ", i + 1, arr[i]);
-      printf ((strcmp (binaryOutput, binary[i]) == 0) ? ("%11s", "pass\n") : (" %11s", "Fail\n"));
-   }
+   PrintResult (arr, binary, DecimalToBinary);
+  
    printf ("\n*****DecimaltoHexadecimal*****\n");
-   for (int i = 0; i < 10; i++) {
-      char* HexOutput = DecimalToHexadecimal (arr[i]);
-      printf ("TestCase%2d: input->%12d ", i + 1, arr[i]);
-      printf ((strcmp (HexOutput, hex[i]) == 0) ? ("%11s", "pass\n") : (" %11s", "Fail\n"));
-   }
+   PrintResult (arr, hex,DecimalToHexadecimal);
+   
        //To get an user input 
       while (1) {
-         int dec = 0;
-         dec = GetInteger ();
-         if (dec != INT_MIN) {
-            char* binary = DecimalToBinary (dec);
-            printf ("Binary value:%s", binary);
-            char* hex = DecimalToHexadecimal (dec);
-            printf ("\nHexadecimal value:%s\n", hex);
-            return 0;
+         char input[100];
+         int number = 0;
+         char* endptr;
+         printf ("\nEnter an integer: ");
+         if (fgets (input, sizeof (input), stdin) != NULL) {
+            errno = 0;
+            number = strtol (input, &endptr, 10);
+            if (endptr == input || *endptr != '\n' && *endptr != '\0' || (number == LONG_MAX || number == LONG_MIN) && errno == ERANGE
+                || number > INT_MAX || number < INT_MIN || input[0] == ' ') {
+               printf ("INVALID!!!\n");
+               
+            }
+            else {
+               printf ("Binary value:%s", DecimalToBinary (number));
+               printf ("\nHexadecimal value:%s\n", DecimalToHexadecimal (number));
+            }
+         }
+         else {
+            printf ("INVALID!!!\n");
          }
       }
       return 0;
