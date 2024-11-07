863d4a95514390b3649ff26ff2fb974640a398ed
Naveenkumar <Naveenkumar.saravanakumar@trumpf.com>
2024-11-07 09:22:02 +0530

    A5 New changes

--- Project21/Project21/Traingit.c
+++ Project21/Project21/Traingit.c
@@ -5,16 +5,16 @@
 // Program.c
 // c program to find the search and sort algorithm.
 // ------------------------------------------------------------------------------------------------
-#include<stdio.h>
-#include<stdbool.h>
-#include<stdlib.h>
-#include<string.h>
-#include<stdint.h>
 #include"Header.h"
 #include<conio.h>
+#include <malloc.h>
+#include<stdbool.h>
+#include<stdint.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-
-void ClrScreen () {
+static void ClrScreen () {
 #ifdef _WIN32
    system ("cls");
 #else
@@ -22,14 +22,12 @@ void ClrScreen () {
 #endif 
 }
 
-
 /// <summary>To get the user input </summary>
-int UserInput (char* context) {
+static int UserInput (char* context) {
    long long int value = 0;
    while (1) {
       printf (context);
-      char arr[13];
-      char* endptr = NULL;
+      char arr[13], * endptr = NULL;
       fgets (arr, 13, stdin);
       char* result = strchr (arr, '\n');// \n checking
       value = strtoll (arr, &endptr, 10);
@@ -44,7 +42,7 @@ int UserInput (char* context) {
 }
 
 /// <summary>To implement the sort and search algorithm </summary>
-int SortAndSearch () {
+static int SortAndSearch () {
    int value = 0;
    do {
       value = UserInput ("\n\n\033[33mEnter the size:\033[0m"); //value varaiable pass dynamic memory allocate
@@ -56,78 +54,79 @@ int SortAndSearch () {
       return 0;
    }
    printf ("\n\033[35mEnter the elements:\n\033[0m");
-   for (int i = 0; i < value; i++) {
-      output[i] = UserInput ("");
-   }
+   for (int i = 0; i < value; i++) output[i] = UserInput ("");
    BubbleSort (output, value);
    printf ("\033[32mRESULT:\033[0m");
-   for (int i = 0; i < value; i++) {
-      printf ("%d ", output[i]);
-   }
+   for (int i = 0; i < value; i++)  printf ("%d ", output[i]);
    int res = BinarySearch (output, value, UserInput ("\n\033[36mEnter the element No to find the index:\033[0m "));
    printf ("\n\033[32mIndex position is: %d \033[0m", res);
    return 0;
 }
 
-/// <summary>Function to run the test cases </summary>
-void TestCases () {
-   printf ("\033[36m\n*****TestCases*****\n\033[0m");
-   int ipArray[][6] = { {20,19,-5,30,21,12 },
-                          {55 ,-214748,47,54,8,4,},
-                          {-3,25,-7,-5,2,-21},
-                          {0,1,2,3,4,5},
-                          {0,-1,-3,-2,-4,-5 },
-                          {-250,60,46,17,15,20},
-                          {223,2000,-287,78,-67,122},
-                          {2,3,4,5,6,7} },
-      OutArray[][6] = { {-5,12,19,20,21,30 },
-                          { -214748 ,4,8,47,54,55},
-                          {-21,-7,-5,-3,2,25},
-                          {0,1,2,3,4,5},
-                          {-5,-4,-3,-2,-1,0},
-                          {-250,15,17,20,46,60},
-                          {-287,-67,78,122,223,2000 },
-                          {2,3,4,5,6,7} },
-      arraySize = sizeof (ipArray) / sizeof (ipArray[0]), //array sizes
-      iptarget[] = { 20,4,-7,2,-3,46,78,6, }, outIndex[] = { 3,1,1,2,2,4,2,4 }, result = 1;//sizes no of elements
-   for (int i = 0; i < arraySize; i++) {
-      int size = sizeof (ipArray[i]) / sizeof (ipArray[i][0]);
+/// <summary>Function to run the Sort test cases </summary>
+static void SortTestCases () {
+   printf ("\033[36m\n*****TestCases*****\n\n**********Sort**********\n\033[0m");
+   int r0[] = { 20,19,-5,30,21,12 }, r1[] = { 55 ,54,8,4, }, r2[] = { -3,-5,2, }, r3[] = { 0,1,2,3,4 }, r4[] = { -1,-3,-2,-4, },
+      r5[] = { -250,60,46,17,15 }, r6[] = { 223,2000,-287,78,-67,122 }, r7[] = { 2,3,4,5 },
+      * jagged[] = { r0,r1,r2,r3,r4,r5,r6,r7 }, size[] = { 6,4,3,5,4,5,6,4 };
+   for (int i = 0; i < 8; i++) {
+      printf ("\n---------------------------------------------------------------------------------------------");
       printf ("\nInputArray:   ");
-      for (int j = 0; j < size; j++) printf ("%d ", ipArray[i][j]);
-      BubbleSort (ipArray[i], size);
+      for (int j = 0; j < size[i]; j++) printf ("%d ", jagged[i][j]);
+      BubbleSort (jagged[i], size[i]);
       printf ("\nSorted Array:  ");
-      for (int j = 0; j < size; j++) printf ("%d ", ipArray[i][j]);
+      for (int j = 0; j < size[i]; j++) printf ("%d ", jagged[i][j]);
       printf ("\nBubble Sort:   ");
-      for (int j = 0; j < size; j++)
-         if (ipArray[i][j] != OutArray[i][j])  result = 0;
-      printf ((result) ? "\033[32mPass\033[0m" : "\033[31mfail\033[0m");
-      int index = BinarySearch (ipArray[i], size, iptarget[i]);
-      printf ("\nKey:   %d\nIndex: %d\nBinary Search: ", iptarget[i], index);
-      printf ((index == outIndex[i]) ? "\033[32mPASS\033[0m" :
-              "\033[31mFAIL\033[0m");
+      int k = 0;
+      for (; k < size[i] - 1; k++)
+         if (jagged[i][k + 1] < jagged[i][k]) {
+            printf ("\033[31mFAIL\033[0m");
+            break;
+         }
+      if (k == size[i] - 1) printf ("\033[32mPASS\033[0m");
    }
+   printf ("\n---------------------------------------------------------------------------------------------");
+}
+
+/// <summary>Function to run the test cases </summary>
+static void TestCases () {
+   int r0[] = { 20,19,-5,30,21,12 }, r1[] = { 55 ,54,8,4, }, r2[] = { -3,-5,2, }, r3[] = { 0,1,2,3,4 },
+      r4[] = { -1,-3,-2,-4, }, r5[] = { -250,60,46,17,15 }, r6[] = { 223,2000,-287,78,-67,122 }, r7[] = { 2,3,4,5 },
+      * jagged[] = { r0,r1,r2,r3,r4,r5,r6,r7 },
+      size[] = { 6,4,3,5,4,5,6,4 },
+      iptarget[] = { 20,54,2,3,-2,17,78,5 },
+      expectedArray[] = { 3,2,2,3,2,2,2,3 };
+   printf ("\033[36m\n*****TestCases*****\n\n**********Binary Search**********\033[0m");
+   for (int i = 0; i < 8; i++) {
+      printf ("\n---------------------------------------------------------------------------------------------");
+      BubbleSort (jagged[i], size[i]);
+      int index = BinarySearch (jagged[i], size[i], iptarget[i]);
+      printf ("\nKey:   %d\nIndex: %d\nBinary Search: ", iptarget[i], index);
+      printf ((index == expectedArray[i]) ? "\033[32mPASS\033[0m" : "\033[31mFAIL\033[0m");
+   }
+   printf ("\n---------------------------------------------------------------------------------------------");
    printf ("\n");
 }
 
 int main () {
    for (;;) {
-      printf ("\n*****CHOOSE*****\n1 = SortAndSearch\n2 = TestCases\n3 = ClearScreen\n4 = Exit\nChoose any option : ");
-      char choice = _getch ();
-         switch (choice) {
-         case '1':
-            SortAndSearch ();
-            break;
-         case '2':
-            TestCases ();
-            break;
-         case '3':
-            ClrScreen ();
-            break;
-         case '4':
-            return 0;
-         default:
-            printf ("\n\033[31mEnter a number between 1-4\n\033[0m");
-         }
+      printf ("\n*****CHOOSE*****\n1 = SortAndSearch\n2 = TestCases\n3 = ClearScreen\n4 = Exit\nChoose any option : \n");
+      switch (_getch ()) {
+      case '1':
+         SortAndSearch ();
+         break;
+      case '2':
+         SortTestCases ();
+         TestCases ();
+         break;
+      case '3':
+         ClrScreen ();
+         break;
+      case '4':
+         return 0;
+      default:
+         printf ("\n\033[31mEnter a number between 1-4\n\033[0m");
       }
+   }
    return 0;
 }
