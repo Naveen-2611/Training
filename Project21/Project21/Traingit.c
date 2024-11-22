f8784a70405e3b60af763e72d735d0f3e8a6f1d5
Naveenkumar <Naveenkumar.saravanakumar@trumpf.com>
2024-11-22 13:03:32 +0530

    Test1

--- Project21/Project21/Traingit.c
+++ Project21/Project21/Traingit.c
@@ -6,7 +6,24 @@
 // Program on main branch.
 // ------------------------------------------------------------------------------------------------
 #include<stdio.h>
-void main () {
-	printf ("Hellow world");
+#include<string.h>
+struct operation {
+   int add;
+   int sub;
+   int multiply;
+   int modulus;
+};
+
+int main () {
+   struct operation operation1;
+   operation1.add = 10 + 10;
+   operation1.sub = 20 - 10;
+   operation1.multiply = 10 * 10;
+   operation1.modulus = 20 % 5;
+
+   printf ("addition:%d\n", operation1.add);
+   printf ("subraction:%d\n", operation1.sub);
+   printf ("multiply:%d\n", operation1.multiply);
+   printf ("modulus:%d\n", operation1.modulus);
+   return 0;
 }
-getch;
